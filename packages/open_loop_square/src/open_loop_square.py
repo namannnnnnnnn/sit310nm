b3d566df2128a52615c2dc0b867c2d7984413a68
namannnnnnnnn <naman9876733288@gmail.com>
2026-05-05 11:16:51 +1000

    Add direct run open loop square code

--- packages/open_loop_square/src/open_loop_square.py
+++ packages/open_loop_square/src/open_loop_square.py
@@ -2,110 +2,86 @@
 
 import rospy
 from duckietown_msgs.msg import Twist2DStamped
-from duckietown_msgs.msg import FSMState
 
 
-class Drive_Square:
+class OpenLoopSquare:
     def __init__(self):
-        # Initialize movement message
+        # Start ROS node
+        rospy.init_node("open_loop_square_direct_node", anonymous=True)
+
+        # Robot command topic
+        self.cmd_topic = "/mybota002822/car_cmd_switch_node/cmd"
+
+        # Publisher sends speed command to robot
+        self.pub = rospy.Publisher(self.cmd_topic, Twist2DStamped, queue_size=1)
+
+        # Movement message
         self.cmd_msg = Twist2DStamped()
 
-        # This is used so robot does not start again and again
-        self.is_moving = False
+        # Wait so publisher can connect properly
+        rospy.sleep(2)
 
-        # Initialize ROS node
-        rospy.init_node('drive_square_node', anonymous=True)
+        rospy.loginfo("Direct open loop square code started")
+        rospy.loginfo("Publishing to: %s", self.cmd_topic)
 
-        # Publisher and subscriber topics for my robot
-        self.pub = rospy.Publisher('/mybota002822/car_cmd_switch_node/cmd',
-                                   Twist2DStamped,
-                                   queue_size=1)
-
-        rospy.Subscriber('/mybota002822/fsm_node/mode',
-                         FSMState,
-                         self.fsm_callback,
-                         queue_size=1)
-
-        rospy.loginfo("Open loop square node started")
-
-    # Robot only moves when lane following is selected from joystick app
-    def fsm_callback(self, msg):
-        rospy.loginfo("State: %s", msg.state)
-
-        if msg.state == "NORMAL_JOYSTICK_CONTROL":
-            self.stop_robot()
-
-        elif msg.state == "LANE_FOLLOWING":
-            if not self.is_moving:
-                rospy.sleep(1)
-                self.move_robot()
-
-    # Sends zero velocity to stop the robot
     def stop_robot(self):
         self.cmd_msg.header.stamp = rospy.Time.now()
         self.cmd_msg.v = 0.0
         self.cmd_msg.omega = 0.0
         self.pub.publish(self.cmd_msg)
 
-    # Move robot forward for selected time
     def move_forward(self, speed, duration):
+        rospy.loginfo("Moving forward")
+
         self.cmd_msg.header.stamp = rospy.Time.now()
         self.cmd_msg.v = speed
         self.cmd_msg.omega = 0.0
         self.pub.publish(self.cmd_msg)
 
-        rospy.loginfo("Moving forward")
         rospy.sleep(duration)
 
         self.stop_robot()
-        rospy.sleep(0.3)
+        rospy.sleep(0.5)
 
-    # Turn robot left for selected time
     def turn_left(self, speed, duration):
+        rospy.loginfo("Turning left")
+
         self.cmd_msg.header.stamp = rospy.Time.now()
         self.cmd_msg.v = 0.0
         self.cmd_msg.omega = speed
         self.pub.publish(self.cmd_msg)
 
-        rospy.loginfo("Turning left")
         rospy.sleep(duration)
 
         self.stop_robot()
-        rospy.sleep(0.3)
+        rospy.sleep(0.5)
 
-    # Robot drives in a square and then stops
-    def move_robot(self):
-        self.is_moving = True
-        rospy.loginfo("Starting open loop square movement")
+    def run_square(self):
+        rospy.loginfo("Starting square movement")
 
-        # Speed values
+        # Tune these values after testing
         forward_speed = 0.25
         turn_speed = 3.0
 
-        # Time values. You can tune these after testing.
+        # Approx values for 1 metre and 90 degree turn
         straight_time = 4.0
         turn_time = 0.55
 
-        # Repeat 4 times to make square
+        # Make 4 sides of square
         for side in range(4):
-            rospy.loginfo("Side %d: forward", side + 1)
+            rospy.loginfo("Side %d", side + 1)
             self.move_forward(forward_speed, straight_time)
 
-            rospy.loginfo("Corner %d: turn", side + 1)
+            rospy.loginfo("Turn %d", side + 1)
             self.turn_left(turn_speed, turn_time)
 
         self.stop_robot()
-        rospy.loginfo("Square movement completed")
-        self.is_moving = False
-
-    # Keep node running
-    def run(self):
-        rospy.spin()
+        rospy.loginfo("Square completed")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
-        duckiebot_movement = Drive_Square()
-        duckiebot_movement.run()
+        robot = OpenLoopSquare()
+        robot.run_square()
     except rospy.ROSInterruptException:
         pass
